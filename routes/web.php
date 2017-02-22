9c833b516da3094e81d7d61d9061e69100940a6d
JiaXin1117 <jiaxin_wp1117@outlook.com>
2017-02-22 16:23:59 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,3 @@
 Route::get('/', function () {
     return File::get(public_path() . '/frontend/index.html');
 });
-
-Route::group(['namespace' => 'Auth', 'prefix' => 'auth'], function() {
-    Route::post('/login', [
-        'as' => 'login.post',
-        'uses' => 'LoginController@postLogin',
-    ]);
-});
-
-Route::group(['namespace' => 'Test', 'prefix' => 'test'], function() {
-    Route::get('/', [
-        'as' => 'test',
-        'uses' => 'TestController@getIndex',
-    ]);
-    
-    Route::get('/welcome', function() {
-        echo "Welcome";
-    });
-});
-
-
-
-
-
-
-
-
-
