664ac2569471a7a2b0166a0e2dddb8ebd7d48251
JiaXin1117 <jiaxin_wp1117@outlook.com>
2017-03-07 05:12:33 +0800

    no message

--- app/Http/Controllers/Admin/ScheduleController.php
+++ app/Http/Controllers/Admin/ScheduleController.php
@@ -22,70 +22,36 @@ class ScheduleController extends Controller
     // Retrieve schedule by date.
     public function get_Retrive_Schedule_By_Date() { 
         $date = Input::get('date');
-        if ($date == NULL) {
-            return response()->json([
-                'state' => 'fail',
-                'data' => 'No Input Data'
-            ]);
-        }
-        
         $cur_date = date_create($date); 
         
-        $result = \App\Models\Res_Times::where('w_h', $this->db_hol_schedule)
-                    ->where('date', date_format($cur_date, 'Y-m-d'))->count(); 
-        if ($result == 0) { // if weekly schedule
-                $result = \App\Models\Res_Times::where('w_h', $this->db_week_schedule)
-                    ->where('date', '<=',  $cur_date)
-                    ->where('day_of_week',  date('w', strtotime($date)))
-                    ->count();
-                if ($result == 0) {
-                    return response()->json([
-                        'state' => 'fail',
-                        'data' => 'No weekly schedule set for this date.'
-                    ]);
-                } else {
-                    $result = \App\Models\Res_Times::where('w_h', $this->db_week_schedule)
-                    ->where('date', '<=',  $cur_date)
-                    ->where('day_of_week',  date('w', strtotime($date)))
-                    ->get();
-                    
-                    $response = array();
-                    foreach ($result as $reservation) {
-                        $temp = array();
-                        $temp['time'] = $reservation->time;
-                        $temp['stop_area'] = Res_Stops::find($reservation->stop_id)->short;
-                        $temp['max_cap'] = Res_Groups::find($reservation->group_id)->max_cap;
-                        $temp['group_id'] = $reservation->group_id;
-                        $temp['schedule_type'] = $reservation->w_h;
+        $cnt = \App\Models\Res_Times::where('date', '<=',  $cur_date)
+            ->where('day_of_week',  date('w', strtotime($date)))
+            ->count();
+        if ($cnt == 0) {
+            return response()->json([
+                'state' => 'fail',
+                'data' => 'No weekly schedule set for this date.'
+            ]);
+        } else {
+            $result = \App\Models\Res_Times::where('date', '<=',  $cur_date)
+            ->where('day_of_week',  date('w', strtotime($date)))
+            ->get();
 
-                        $response[] = $temp;
-                    }
-                    return response()->json([
-                        'state' => 'success',
-                        'data' => $response
-                    ]);
-                }
-                
-                
-        } else {    // if holiday schedule
-                $result = \App\Models\Res_Times::where('w_h', $this->db_hol_schedule)
-                    ->where('date', date_format($cur_date, 'Y-m-d'))->get(); 
-            
-                $response = array();
-                foreach ($result as $reservation) {
-                    $temp = array();
-                    $temp['time'] = $reservation->time;
-                    $temp['stop_area'] = Res_Stops::find($reservation->stop_id)->short;
-                    $temp['max_cap'] = Res_Groups::find($reservation->group_id)->max_cap;
-                    $temp['group_id'] = $reservation->group_id;
-                    $temp['schedule_type'] = $reservation->w_h;
-                    
-                    $response[] = $temp;
-                }
-                return response()->json([
-                    'state' => 'success',
-                    'data' => $response
-                ]);
+            $response = array();
+            foreach ($result as $reservation) {
+                $temp = array();
+                $temp['time'] = $reservation->time;
+                $temp['stop_area'] = Res_Stops::find($reservation->stop_id)->short;
+                $temp['max_cap'] = Res_Groups::find($reservation->group_id)->max_cap;
+                $temp['group_id'] = $reservation->group_id;
+                $temp['schedule_type'] = $reservation->w_h;
+
+                $response[] = $temp;
+            }
+            return response()->json([
+                'state' => 'success',
+                'data' => $response
+            ]);
         }
         
         return response()->json([
