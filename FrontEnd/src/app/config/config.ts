ff32caedbfe6198ca352940efe507f0c1ead8eaf
JiaXin1117 <jiaxin_wp1117@outlook.com>
2017-05-20 08:03:42 +0800

    add reservation

--- FrontEnd/src/app/config/config.ts
+++ FrontEnd/src/app/config/config.ts
@@ -1,3 +1,2 @@
- export const BACKEND_SERVER_URL = "http://localhost/TripperBus/";
-// export const BACKEND_SERVER_URL = "http://54.214.196.171/TripperBus/public/";
-
+// export const BACKEND_SERVER_URL = "http://localhost/TripperBus/";
+ export const BACKEND_SERVER_URL = "http://54.214.196.171/TripperBus/public/";
