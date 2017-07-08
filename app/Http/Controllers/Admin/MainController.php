3450d0491cfdc8faaa9d69912e1be0c6a2d56c76
JiaXin1117 <jiaxin_wp1117@outlook.com>
2017-07-08 22:57:52 +0800

    3. In Search Page, Time field should not be in 24 hour format. make it as 4:23 PM instead of 16:23:00

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -308,17 +308,15 @@ class MainController extends Controller
             $searchKey = 'created_at';
         }
 
-        Res_Reservations::unguard();
-
         if ($caseSensitive) {
-            $result = Res_Reservations::join('res_times', 'time_id', '=', 'res_times.id')
+            $reservations = Res_Reservations::join('res_times', 'time_id', '=', 'res_times.id')
             ->join('res_stops', 'res_times.stop_id', '=', 'res_stops.id')
             ->where('res_reservations.valid', 1)
             ->where('res_reservations.'.$searchKey, 'like', '%'.$searchVal.'%')
             ->get(['res_reservations.created_at AS Date Made', 'res_reservations.*', 'res_times.time', 'res_stops.short AS stop'])
             ->toarray();
         } else {
-            $result = Res_Reservations::join('res_times', 'time_id', '=', 'res_times.id')
+            $reservations = Res_Reservations::join('res_times', 'time_id', '=', 'res_times.id')
             ->join('res_stops', 'res_times.stop_id', '=', 'res_stops.id')
             ->where('res_reservations.valid', 1)
             ->whereRaw('LOWER(res_reservations.`'.$searchKey.'`) like "%'.strtolower($searchVal).'%"')
@@ -326,11 +324,13 @@ class MainController extends Controller
             ->toarray();
         }
 
-        Res_Reservations::reguard();
+        foreach($reservations as $key => $value) {
+            $reservations[$key]['time'] = date("g:i A", strtotime($reservations[$key]['time']));
+        }
 
         return response()->json([
             'state' => 'success',
-            'data' => $result
+            'data' => $reservations,
         ]);
     }
 
