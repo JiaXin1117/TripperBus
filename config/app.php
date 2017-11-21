d3d74d3f8ea6062d40cd9acc27c67a0681eb8fea
JiaXin1117 <jiaxin_wp1117@outlook.com>
2017-11-22 06:55:19 +0800

    cors back

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Aloha\Twilio\Support\Laravel\ServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
